2113f82d9b3e2bfd2ac02dba073efd44e55b386a
Ivan Alferov <aia@bileter.ru>
2015-08-03 14:31:47 +0300

    SVG generation

---
+++ svg/svgserver.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/ajstarks/svgo"
+	"net/http"
+	"time"
+	"log"
+)
+
+func main() {
+
+	http.Handle("/", http.HandlerFunc(index))
+	http.Handle("/circle", http.HandlerFunc(circle))
+	err := http.ListenAndServe(":2003", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Add("Refresh", "5")
+	w.Write([]byte(`<img src="/circle" />`))
+}
+
+func circle(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	width := 500
+	height := 500
+	canvas := svg.New(w)
+	canvas.Start(width, height)
+	canvas.Circle(width/2, height/2, 100)
+	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+	canvas.Text(width/2, height/4, time.Now().Format("Jan _2 15:04:05 2006"), "text-anchor:middle;font-family:'Droid';font-size:30px;fill:blue")
+	canvas.End()
+}
+
