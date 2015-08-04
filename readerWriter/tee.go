dc397c939596204f9da015ad9298a30ac2573027
Ivan A-R <ivan@tuxotronic.org>
2015-08-04 09:53:36 +0300

    TeeReader sample

---
+++ readerWriter/tee.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"time"
+	"strings"
+	"io"
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+
+	s := time.Now().String()
+	in := strings.NewReader(s)
+
+	// Write pdf to file
+	out, err := os.OpenFile("output.txt", os.O_CREATE | os.O_WRONLY, 0666)
+	chk(err)
+	defer out.Close()
+
+	tee := io.TeeReader(in, out)
+
+	data, err := ioutil.ReadAll(tee)
+	chk(err)
+	fmt.Println(string(data))
+}
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
