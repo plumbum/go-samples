4d1af0c4d696d10c69f58cc28c27fc41812b00bf
Ivan A-R <ivan@tuxotronic.org>
2015-08-06 15:16:57 +0300

    Get API key from environment variables

--- geoMap/geomap.go
+++ geoMap/geomap.go
@@ -9,50 +9,15 @@ import (
 
 func main() {
 
-	api := ""
-	cfg, err := os.Open("api.cfg")
-	if err == nil {
-		defer cfg.Close()
-		fmt.Fscanln(cfg, &api)
-	}
+	api := os.Getenv("YANDEX_MAP_API_KEY")
+	fmt.Println("API KEY:", api)
 
 	address := "СПб, пр. Пятилеток, 15-1"
+	fmt.Println("Dial address:", address)
 
 	pn, err := yandexmap.Geocode(address, api)
 	fmt.Println(err)
 	spew.Dump(pn)
-
-	/*
-	var gc geo.GoogleGeocoder
-
-	pt, err := gc.Geocode(address)
-	chk(err)
-
-	spew.Dump(pt)
-	*/
-
-
-	/*
-	var rmap map[string]*json.RawMessage
-	json.Unmarshal(data, &rmap)
-	spew.Dump(rmap)
-	*/
-
-	/*
-	spew.Dump(resp.StatusCode)
-	spew.Dump(resp.Status)
-	spew.Dump(resp.ContentLength)
-	spew.Dump(resp.Proto)
-	spew.Dump(resp.Header)
-	spew.Dump(resp.Cookies())
-	spew.Dump(resp.Close)
-	*/
-
-
-	/*
-	fmt.Println(string(data))
-	*/
-
 }
 
 func chk(err error) {
---
+++ geoMap/run.geomap.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# export YANDEX_MAP_API_KEY="<define api key here>"
+go run geomap.go
+
