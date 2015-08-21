8e900306098a01d609e982e39d1398b9605b8cdc
Ivan Alferov <aia@bileter.ru>
2015-08-21 13:07:50 +0300

    change slice demo

--- slices/slieces.go
+++ slices/slieces.go
@@ -1,18 +1,31 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
+const InitialCapacity = 8
 
 func main() {
 
-	s1 := []int{1, 2}
-	s2 := []int{3, 4}
-	fmt.Println("s1", s1)
-	fmt.Println("s2", s2)
+	// Создадим пустой слайс но с начальной ёмкостью.
+	// Можно поигратсься с ёмкость, что бы посмотреть на реакцию слайса
+	slice := make([]int, 0, InitialCapacity)
+	fmt.Printf("SOURCE: cap=%d; len=%d; %v\r\n", cap(slice), len(slice), slice)
+
+	// Начинаем заполнять слайс
+	for i:=0; i<32; i++ {
+		// append при недостатке ёмкости увеличивает её вдвое
+		slice = append(slice, i)
+		fmt.Printf("%5d: cap=%d; len=%d; %v\r\n", i, cap(slice), len(slice), slice)
+	}
 
 	// Append element to sliece
-	s2 = append(s2, 5)
+	slice = append(slice, 99)
+	fmt.Printf("Append element: cap=%d; len=%d; %v\r\n", cap(slice), len(slice), slice)
 	// Append sliece to sliece
-	s3 := append(s1, s2...)
-
-	fmt.Println("s2", s2)
-	fmt.Println("s3", s3)
+	aslice := []int{201,202,203,204}
+	slice = append(slice, aslice...)
+	fmt.Printf("Append slice: cap=%d; len=%d; %v\r\n", cap(slice), len(slice), slice)
 }
+
