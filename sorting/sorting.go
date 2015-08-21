91ad139c85ccbb961eb56126a3fdfed6396271dd
Ivan A-R <ivan@tuxotronic.org>
2015-08-21 15:18:33 +0300

    sort slices

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -6,13 +6,21 @@ import (
 )
 
 func main() {
+	fmt.Println("Sort strings")
 	a := [...]string{"first", "second", "third", "fourth", "fifth"}
-	fmt.Println(a)
-	sort.Strings(a[0:len(a)])
-	fmt.Println(a)
+	fmt.Println("  Unsorted:", a)
+	sort.Strings(a[:])
+	fmt.Println("    Sorted:", a)
 
-	i := [...]int{7, 2, 10, 33, 4, -9, 0, 123, 74}
-	fmt.Println(i)
-	sort.Ints(i[0:len(i)])
-	fmt.Println(i)
+	fmt.Println("Sort strings")
+	nums := []string{"2", "7", "10", "33", "101"}
+	fmt.Println("  Unsorted:", nums)
+	sort.Strings(nums)
+	fmt.Println("    Sorted:", nums)
+
+	fmt.Println("Sort integers")
+	ints := []int{7, 2, 10, 33, 4, -9, 0, 123, 74}
+	fmt.Println("  Unsorted:", ints)
+	sort.Ints(ints)
+	fmt.Println("    Sorted:", ints)
 }
