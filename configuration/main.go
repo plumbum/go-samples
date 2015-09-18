907d00b7cc392cd940839aab6bb1964f2433e6bb
Ivan A-R <ivan@tuxotronic.org>
2015-09-18 17:20:22 +0300

    config onion: get command line parametrs

--- configuration/main.go
+++ configuration/main.go
@@ -4,8 +4,13 @@ import (
 	"github.com/fzerorubigd/onion"
 	_ "github.com/fzerorubigd/onion/tomlloader"
 	_ "github.com/fzerorubigd/onion/yamlloader"
+	_ "github.com/fzerorubigd/onion/flagslayer"
 	"fmt"
 	"strings"
+	"log"
+	"github.com/fzerorubigd/onion/flagslayer"
+	"flag"
+	"os"
 )
 
 type User struct {
@@ -15,16 +20,38 @@ type User struct {
 
 func main() {
 
+	var err error
+
 	cfg := onion.New()
-	l1 := onion.NewFileLayer("configuration/config.yaml")
-	l2 := onion.NewFileLayer("configuration/config.toml")
+
+	// Load YAML config
+	l1 := onion.NewFileLayer("config.yaml")
+	err = cfg.AddLayer(l1)
+	check(err)
+
+	// Load TOML config
+	l2 := onion.NewFileLayer("config.toml")
+	err = cfg.AddLayer(l2)
+	check(err)
+
+	// Get ENV variables
 	l3 := onion.NewEnvLayer("HOME")
+	err = cfg.AddLayer(l3)
+	check(err)
 
-	cfg.AddLayer(l1)
-	cfg.AddLayer(l2)
-	cfg.AddLayer(l3)
+	// Get command line flags
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	l4 := flagslayer.NewFlagLayer(fs)
+	l4.SetString("home", "home", "none", "Set home directory")
+	l4.SetBool("enable", "enable", false, "Enable demo")
+	err = cfg.AddLayer(l4)
+	check(err)
 
+	// Print results
 	fmt.Println(strings.Repeat("=", 80))
+
+	fmt.Println(cfg.GetBool("enable"))
+
 	fmt.Println(cfg.GetInt("first"))
 	fmt.Println(cfg.GetInt("second"))
 	fmt.Println(cfg.GetStringSlice("list"))
@@ -38,4 +65,10 @@ func main() {
 	fmt.Println(cfg.GetString("HOME"))
 
 	fmt.Println(strings.Repeat("-", 80))
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
