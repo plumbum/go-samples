e01d8c1ec09c3de5b390b5059fb5449a2f7ae0be
Ivan A-R <ivan@tuxotronic.org>
2016-03-04 12:43:05 +0300

    Example of package github.com/fiam/gounidecode/unidecode

--- Basic/codingCp1251/main.go
+++ Basic/codingCp1251/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"fmt"
 	"bytes"
+	"github.com/fiam/gounidecode/unidecode"
 )
 
 // Encoding UTF-8 to Cp1251 and back
@@ -17,23 +18,29 @@ func main() {
 	decoder := charmap.Windows1251.NewDecoder()
 
 	inUtf8 := "Ёжики пушистые 好 ἱερογλυφικὰ γράμματ"
+	// inUtf8 := "Ёжики пушистые"
 
 	sr := strings.NewReader(inUtf8)
 	tr := transform.NewReader(sr, encoder)
 	inCp1251, err := ioutil.ReadAll(tr)
 	if err != nil {
-		panic(err)
+		fmt.Println("Encoding error: ", err)
 	}
 
 	srBack := bytes.NewReader(inCp1251)
 	trBack := transform.NewReader(srBack, decoder)
 	outUtf8, err := ioutil.ReadAll(trBack)
 	if err != nil {
-		panic(err)
+		fmt.Println("Decoding error: ", err)
 	}
 
 	fmt.Println("Source UTF8:", inUtf8)
 	fmt.Println("CP1251:", inCp1251, string(inCp1251))
 	fmt.Println("Result UTF8:", string(outUtf8))
+
+	fmt.Println(strings.Repeat("=", 80))
+	fmt.Println("Test https://github.com/fiam/gounidecode")
+	fmt.Println("Original: ", inUtf8)
+	fmt.Println("Translit: ", unidecode.Unidecode(inUtf8))
 }
 
