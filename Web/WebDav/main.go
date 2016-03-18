fa5c66e0a8392488001e2fc2c0d4de3281ae91ce
Ivan A-R <ivan@tuxotronic.org>
2016-03-18 17:56:56 +0300

    simple WebDav server sample

---
+++ Web/WebDav/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"github.com/abbot/go-http-auth"
+	"golang.org/x/net/webdav"
+)
+
+func GetSecret(user, realm string) string {
+	log.Printf("User: %s; Realm: %s\n", user, realm)
+	if user == "john" {
+		// password is "hello"
+		return "$2y$05$DEKfYsAg/ROn8n4TXE3J3OxlnbHr6RyFVd.RX1OMsLCwVOPhpUuKS" // bcrypt
+	}
+	return ""
+}
+
+func main() {
+	authenticator := auth.NewBasicAuthenticator("webdav", GetSecret)
+
+	h := new(webdav.Handler)
+	root, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	h.FileSystem = webdav.Dir(root)
+	h.LockSystem = webdav.NewMemLS()
+	h.Prefix = "/dav/"
+	h.Logger = func(r *http.Request, e error) {
+		log.Printf("%s(%s): %s", r.Method, r.RemoteAddr, r.RequestURI)
+		if err != nil {
+			log.Println("[ERROR] ", err)
+		}
+	}
+
+	//then use the Handler.ServeHTTP Method as the http.HandleFunc
+	http.HandleFunc("/dav/", authenticator.Wrap(
+		func(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+			h.ServeHTTP(w, &r.Request)
+			log.Print("...done")
+		}))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "text/html; charset=UTF-8")
+		w.Write([]byte(fmt.Sprintf(`Time: %s`, time.Now().String())))
+	})
+
+	log.Println("Listen at :5555")
+	http.ListenAndServe(":5555", nil)
+}
